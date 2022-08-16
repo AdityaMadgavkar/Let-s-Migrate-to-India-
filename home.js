27b1832befae904e943ca3b331030d538aa92714
AdityaMadgavkar <96823625+AdityaMadgavkar@users.noreply.github.com>
2022-08-16 21:01:20 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,68 @@
+
+function backHome(){
+    window.location="home.html";
+}
+function toEd(){
+    window.location="education.html";
+}
+function toFe(){
+    window.location="festivals.html";
+}
+function toTr(){
+    window.location="transport.html";
+}
+function about(){
+    window.location="https://en.wikipedia.org/wiki/India";
+}
+function toLa(){
+    window.location="language.html";
+}
+function solveed(){
+    document.getElementById("s1").innerHTML="India has forgein boards like Cambridge International Education (CIE), and International General Certificate of Secondary Education (IGCSE), that provides education in worldwide used English.";
+    document.getElementById("s2").innerHTML="India has 513 government universities.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1").style.color="yellow";
+    document.getElementById("s2").style.color="yellow";
+}
+function solvet(){
+    document.getElementById("s1t").innerHTML="Government of India has permitted 100% Foreign Direct Investment (FDI) in construction, operation and maintenance of Indian Railways, making it a preferable option over rickshaws.";
+    document.getElementById("s2t").innerHTML="The existing minimum fare of Rs.65 for a 5 km local train journey will be reduced to Rs.30.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1t").style.color="yellow";
+    document.getElementById("s2t").style.color="yellow";
+}
+function solvef(){
+    document.getElementById("s1f").innerHTML="Try to learn the reason and origination of traditions.";
+    document.getElementById("s2f").innerHTML="Everybody has the right to celebrate their festival of choice without violating the fundamental rights of others.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1f").style.color="yellow";
+    document.getElementById("s2f").style.color="yellow";
+}
+            function toCu(){
+                window.location="customs.html";
+            }
+            function toHe(){
+                window.location="health.html";
+            }
+            function toFo(){
+                window.location="food.html";
+            }
+
+function solvela(){
+    document.getElementById("s1l").innerHTML="Make friends who will be able to teach you the language, or select a neighbourhood where English is spoken.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1l").style.color="yellow";
+}
+
+function solvefo(){
+    document.getElementById("s1fo").innerHTML="Branches of international food companies are found throughout India.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1fo").style.color="yellow";
+}
+function solvehe(){
+    document.getElementById("s1h").innerHTML="Migrate to an area where green energy is being used.";
+    document.getElementById("s2h").innerHTML="Migrate to a place at an high altitude.";
+    document.getElementById("chall").innerHTML="<span> Solutions</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fa fa-sort-desc'></i>";
+    document.getElementById("s1h").style.color="yellow";
+    document.getElementById("s2h").style.color="yellow";
+}
